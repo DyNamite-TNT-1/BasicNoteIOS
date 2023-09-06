022f5b0f261ba9acd21e3115d5cf609905f7dfde
lemauanhduc <tdhuaco372001@gmail.com>
2023-09-06 10:33:47 +0700

    Initial Commit

---
+++ BasicNoteIOS/BasicNoteIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicNoteIOSApp.swift
+//  BasicNoteIOS
+//
+//  Created by hanbiro on 9/6/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicNoteIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
